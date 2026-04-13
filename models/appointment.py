10c6872486f7b3e8bc14246fe3008ff5eab1bc23
sayed ehab <sehab5147@gmail.com>
2026-04-14 01:59:03 +0200

    add appointment states logic and history tracking

--- models/appointment.py
+++ models/appointment.py
@@ -1,4 +1,5 @@
 from odoo import fields,models,api, _
+from datetime import date
 
 
 class HospitalAppointment(models.Model):
@@ -10,7 +11,13 @@ class HospitalAppointment(models.Model):
     _inherit=['mail.thread','mail.activity.mixin']
 
 
-    patient_id=fields.Many2one('hospital.patient',string="Patient_Name", tracking=True)
+    patient_id = fields.Many2one('hospital.patient', string="Patient", tracking=True, states={'draft': [('readonly', False)]})    
+
+    state = fields.Selection([
+        ('draft', 'Draft'),      
+        ('done', 'Done'),        
+        ('cancel', 'Cancelled'), 
+    ], string='Status', default='draft', tracking=True)
 
     age=fields.Integer(string="Age", related='patient_id.age', readonly=True)
 
@@ -20,8 +27,7 @@ class HospitalAppointment(models.Model):
         ('other','Other'),
     ], string='Gender', related='patient_id.gender', readonly=True)
 
-    appointment_time=fields.Date(string="Appointment Time", default=fields.Datetime.now)
-
+    appointment_time = fields.Date(string="Appointment Time", default=fields.Date.context_today)
     booking_time=fields.Date(string="Booking_Date", default=fields.Date.context_today, readonly=True)
 
 
@@ -29,12 +35,16 @@ class HospitalAppointment(models.Model):
 
     appointment_serial = fields.Char(string="Appointment ID", readonly=True, default=lambda self: _('New_Apppointment'))
 
+
+    history_ids = fields.One2many('appointment.history', 'appointment_id', string='History')
+    
     @api.model
     def create(self, vals):
         if vals.get('appointment_serial', _('New_Apppointment')) == _('New_Apppointment'):
             vals['appointment_serial'] = self.env['ir.sequence'].next_by_code('hospital.appointment.sequence') or _('New_Apppointment')
         res = super().create(vals)
         return res
+
 
 
     presciption_count=fields.Integer(string="Preciptions", compute="compute_presciption_count")
@@ -61,3 +71,67 @@ class HospitalAppointment(models.Model):
             'target' : 'current',
             'type' :'ir.actions.act_window'
         }    
+
+
+
+
+    def action_check_today_appointments(self):
+
+            today_date=date.today()
+            appiontments_today=self.search([
+                ('appointment_time', '=', today_date),
+                ('state', '=', 'draft')
+
+            ])
+            for rec in appiontments_today:
+                rec.message_post(body="This Appointment is Today")    
+
+    def create_history(self,old_state,new_state,reason):
+        for rec in self:
+            self.env['appointment.history'].create({
+                'appointment_id':rec.id,
+                'old_state':old_state,
+                'new_state':new_state,
+                'reason':reason,
+            })        
+
+    def action_done(self):
+        for rec in self:
+            rec.create_history(rec.state, 'done', "Appointment completed successfully")
+            rec.state = 'done'
+
+    def action_cancel(self):
+        for rec in self:
+            rec.create_history(rec.state, 'cancel', "Patient cancelled or missed the slot")
+            rec.state = 'cancel'
+
+    def action_reset_to_draft(self):
+        for rec in self:
+            rec.create_history(rec.state, 'draft', "Reset for corrections")
+            rec.state='draft'       
+
+
+    
+    def action_view_history(self):
+        return {
+            'name': 'Appointment History',
+            'type': 'ir.actions.act_window',
+            'res_model': 'appointment.history',
+            'view_mode': 'tree',
+            'domain': [('appointment_id', '=', self.id)],
+            'context': {'default_appointment_id': self.id},
+            'target': 'current',
+        }   
+
+    @api.model
+    def cancel_passed_appointmemnts(self):
+        today=fields.Date.today()
+
+        passed_appointments=self.search([
+            ('appointment_time','<',today),
+            ('state','=','draft')
+        ])   
+
+        for rec in passed_appointments:
+            rec.create_history('draft','cancel',"Auto-cancelled: Appointment date has passed.")
+            rec.state='cancel'
